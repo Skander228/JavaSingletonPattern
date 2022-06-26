105e88456ef4138f71f8fced44a682d35a55c795
Skander228 <kkhemiriskander@gmail.com>
2022-06-26 22:54:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+class Singleton {
+    private Singleton() {}
+
+    public String str;
+
+    private static Singleton instance = null;
+
+    public static Singleton getSingleInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
